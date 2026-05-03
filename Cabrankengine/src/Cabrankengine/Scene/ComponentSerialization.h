0c8cfded30ee52135ad0b13a17cc740e5ef7d94f
cabranca <joaquincabrera98@gmail.com>
2026-05-03 18:48:33 -0300

    Add guards for webAssembly

--- Cabrankengine/src/Cabrankengine/Scene/ComponentSerialization.h
+++ Cabrankengine/src/Cabrankengine/Scene/ComponentSerialization.h
@@ -107,7 +107,9 @@ inline void from_json(const nlohmann::json& j, CSprite& c) {
     j.at("tint").get_to(c.Tint);
     j.at("tiling_factor").get_to(c.TilingFactor);
 
+#ifndef CBK_WASM
     c.Texture = rendering::Texture2D::create(c.Path);
+#endif
 }
 
 inline void to_json(nlohmann::json& j, const CPhongModel& c) {
@@ -118,7 +120,9 @@ inline void to_json(nlohmann::json& j, const CPhongModel& c) {
 inline void from_json(const nlohmann::json& j, CPhongModel& c) {
     j.at("path").get_to(c.Path);
 
+#ifndef CBK_WASM
     c.Res = scene::Model<rendering::PhongMaterial>::create(c.Path);
+#endif
 }
 
 inline void to_json(nlohmann::json& j, const CPBRModel& c) {
@@ -129,7 +133,9 @@ inline void to_json(nlohmann::json& j, const CPBRModel& c) {
 inline void from_json(const nlohmann::json& j, CPBRModel& c) {
     j.at("path").get_to(c.Path);
 
+#ifndef CBK_WASM
     c.Res = scene::Model<rendering::PBRMaterial>::create(c.Path);
+#endif
 }
 
 inline void to_json(nlohmann::json& j, const CText& c) {
