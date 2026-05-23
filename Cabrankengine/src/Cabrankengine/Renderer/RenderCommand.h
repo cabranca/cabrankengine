2cb271dad6e003526b006bdbe3042c869715b566
cabranca <joaquincabrera98@gmail.com>
2026-05-23 14:52:13 -0300

    Fix RendererAPI leak

--- Cabrankengine/src/Cabrankengine/Renderer/RenderCommand.h
+++ Cabrankengine/src/Cabrankengine/Renderer/RenderCommand.h
@@ -12,51 +12,33 @@ namespace cbk::rendering {
 	class RenderCommand {
 	  public:
 		// Initializes the RendererAPI. This should be called once at the start of the application.
-		static void init() {
-			s_RendererAPI->init();
-		}
+		static void init();
 
-		static void shutdown() {
-			s_RendererAPI->shutdown();
-		}
+		static void shutdown();
 
 		// Sets the color used to clear the screen.
-		static void setClearColor(const math::Vector4& color) {
-			s_RendererAPI->setClearColor(color);
-		}
+		static void setClearColor(const math::Vector4& color);
 
 		// Clears the screen with the previously set clear color.
-		static void clear() {
-			s_RendererAPI->clear();
-		}
+		static void clear();
 
-		static void beginFrame() {
-			s_RendererAPI->beginFrame();
-		}
+		static void beginFrame();
 
 		// Draws the vertex array vertices in order
-		static void draw(const Ref<GeometryDescriptor>& desc) {
-			s_RendererAPI->draw(desc);
-		}
+		static void draw(const Ref<GeometryDescriptor>& desc);
 
 		// Draws the indexed vertices from the vertex array. Transform defaults to identity for
 		// batched 2D / text submissions whose vertices are already in world space.
 		static void drawIndexed(const Ref<Material>& material, const Ref<GeometryDescriptor>& desc,
-		                        const math::Mat4& transform = math::identityMat(), uint32_t indexCount = 0) {
-			s_RendererAPI->drawIndexed(material, desc, transform, indexCount);
-		}
+		                        const math::Mat4& transform = math::identityMat(), uint32_t indexCount = 0);
 
-		static void endFrame() {
-			s_RendererAPI->endFrame();
-		}
+		static void endFrame();
 
-		static void setViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) {
-			s_RendererAPI->setViewport(x, y, width, height);
-		}
+		static void setViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height);
 
-		static RendererAPI* getRendererAPI() { return s_RendererAPI; }
+		static RendererAPI* getRendererAPI();
 
 	  private:
-		static RendererAPI* s_RendererAPI; // This is a pointer to the RendererAPI instance that will be used for rendering commands.
+		static Scope<RendererAPI> s_RendererAPI; // This is a pointer to the RendererAPI instance that will be used for rendering commands.
 	};
 } // namespace cbk::rendering
