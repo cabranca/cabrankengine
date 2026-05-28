71708530094563945ac9357d23c149b33ef491d0
cabranca <joaquincabrera98@gmail.com>
2026-05-28 15:51:22 -0300

    Increase max entities

--- Cabrankengine/src/Cabrankengine/ECS/Common.h
+++ Cabrankengine/src/Cabrankengine/ECS/Common.h
@@ -6,7 +6,7 @@ namespace cbk::ecs {
 
 	using Entity = uint32_t;
 
-	constexpr Entity MAX_ENTITIES = 10000;
+	constexpr Entity MAX_ENTITIES = 20000;
 	constexpr Entity INVALID_ENTITY = std::numeric_limits<Entity>::max();
 	constexpr size_t MAX_COMPONENTS = 64;
 
